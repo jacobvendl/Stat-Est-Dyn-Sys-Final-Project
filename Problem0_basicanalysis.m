dbfef1017bda34a59a130c63e0b5f6d57f0ec303
Jacob <jacob.vendl@colorado.edu>
2019-12-07 12:15:22 -0700

    basic system analysis working

--- Problem0_basicanalysis.m
+++ Problem0_basicanalysis.m
@@ -38,17 +38,21 @@ dx0 = [0, 0.075, 0, -0.021]'; %come back and get this with MC
 %STEP TWO - simulate perturbed ground truth state using ode45
 s0 = x0 + dx0;
 opts = odeset('RelTol',1e-12,'AbsTol',1e-12);
-[T, x_star] = ode45(@(t,s)orbit_prop_func(t,s),tvec,s0,opts);
+[T, x_perturbed] = ode45(@(t,s)orbit_prop_func(t,s),tvec,s0,opts);
+x_perturbed = x_perturbed';
+
+[T,x_star] = ode45(@(t,s)orbit_prop_func(t,s),tvec,x0,opts);
+x_star = x_star';
 
 %STEP THREE = simulate ground truth measurements using ode45 result
-X=x_star(:,1); Y=x_star(:,3); XD=x_star(:,2); YD=x_star(:,4);
+X=x_star(1,:); Y=x_star(3,:); XD=x_star(2,:); YD=x_star(4,:);
 Xs = zeros(12,length(T));
 Ys = zeros(12,length(T));
 XDs = zeros(12,length(T));
 YDs = zeros(12,length(T));
-rho_per = zeros(12,length(T));
-rhoDot_per = zeros(12,length(T));
-phi_per = zeros(12,length(T));
+rho = zeros(12,length(T));
+rhoDot = zeros(12,length(T));
+phi = zeros(12,length(T));
 y_star = zeros(36,length(T));
 %now simulate the measurements for all time
 for i=1:12 %stations
@@ -63,7 +67,7 @@ for i=1:12 %stations
         YDs(i,t) = rE*wE*cos(wE*currentTime + theta);
         
         %peform check at given time to see if s/c is visible
-        phi_per(i,t) = atan2((Y(t)-Ys(i,t)),(X(t)-Xs(i,t)));
+        phi(i,t) = atan2((Y(t)-Ys(i,t)),(X(t)-Xs(i,t)));
         thetaCheck = atan2(Ys(i,t),Xs(i,t));
         if (thetaCheck-pi/2) > (thetaCheck+pi/2)
             upperBound = thetaCheck-pi/2;
@@ -72,21 +76,21 @@ for i=1:12 %stations
             upperBound = thetaCheck+pi/2;
             lowerBound = thetaCheck-pi/2;
         end
-        if (lowerBound <= phi_per(i,t) && phi_per(i,t) <= upperBound) ...
-                || (lowerBound-2*pi <= phi_per(i,t) && phi_per(i,t)<=upperBound-2*pi)... %accomodate phi wrapping
-                || (lowerBound+2*pi <= phi_per(i,t) && phi_per(i,t)<=upperBound+2*pi)
+        if (lowerBound <= phi(i,t) && phi(i,t) <= upperBound) ...
+                || (lowerBound-2*pi <= phi(i,t) && phi(i,t)<=upperBound-2*pi)... %accomodate phi wrapping
+                || (lowerBound+2*pi <= phi(i,t) && phi(i,t)<=upperBound+2*pi)
             
-            rho_per(i,t) = sqrt((X(t)-Xs(i,t))^2 + (Y(t)-Ys(i,t))^2);
-            rhoDot_per(i,t) = ((X(t)-Xs(i,t))*(XD(t)-XDs(i,t)) + (Y(t)-Ys(i,t))*(YD(t)-YDs(i,t)))...
-                / rho_per(i,t);
+            rho(i,t) = sqrt((X(t)-Xs(i,t))^2 + (Y(t)-Ys(i,t))^2);
+            rhoDot(i,t) = ((X(t)-Xs(i,t))*(XD(t)-XDs(i,t)) + (Y(t)-Ys(i,t))*(YD(t)-YDs(i,t)))...
+                / rho(i,t);
         else
-            rho_per(i,t) = nan;
-            rhoDot_per(i,t) = nan;
-            phi_per(i,t)=nan;
+            rho(i,t) = nan;
+            rhoDot(i,t) = nan;
+            phi(i,t)=nan;
         end
-        y_star(3*i-2,t) = rho_per(i,t);
-        y_star(3*i-1,t) = rhoDot_per(i,t);
-        y_star(3*i,t) = phi_per(i,t);
+        y_star(3*i-2,t) = rho(i,t);
+        y_star(3*i-1,t) = rhoDot(i,t);
+        y_star(3*i,t) = phi(i,t);
     end
 end
 
@@ -103,12 +107,12 @@ for t=1:length(T)
     dx_lin = horzcat(dx_lin, dx(:,t));
     
     %propagate dx forward in time
-    [F, Omega] = F_Gamma_variant(dx(1,t),dx(3,t));
+    [F, Omega] = F_Gamma_variant(X(t),Y(t));
     dx(:,t+1) = F * dx(:,t) ;
     
     %loop through the stations and simulate linearized measurements
     for i=1:12
-        if ~isnan(rho_per(i,t))
+        if ~isnan(rho(i,t))
             H = H_variant(X(t),XD(t),Y(t),YD(t),Xs(i,t),XDs(i,t),Ys(i,t),YDs(i,t));
             dy(:,t) = H*dx(:,t);
             dy_lin(3*i-2:3*i,t) = dy(:,t);
@@ -118,30 +122,89 @@ for t=1:length(T)
     end
 end
 %add dx_lin to x_star to get simulated noisy ground truth states
-x_sim = x_star + dx_lin';
+x_sim = x_star + dx_lin;
 y_sim = y_star + dy_lin;
 
+x_sim_test = x_star + dx_lin;
 
 
-%plots for a single simulation instance, showing the noisy simulated ground
-%truth states
+%states versus time for nonlinear dynamics
 figure; hold on;
-sgtitle(sprintf('Noisy Simulated Ground Truth States \n dx =[%.4fkm %.4fkm/s %.4fkm %.4fkm/s]',dx0(1),dx0(2),dx0(3),dx0(4)))
+sgtitle('States vs. Time, Full Nonlinear Dynamics Simulation')
+subplot(4,1,1); hold on; grid on; grid minor;
+plot(tvec,x_star(1,:),'-')
+ylabel('X [km]')
+subplot(4,1,2); hold on; grid on; grid minor; 
+plot(tvec,x_star(2,:),'-')
+ylabel('Xdot [km/s]')
+subplot(4,1,3); hold on; grid on; grid minor; 
+plot(tvec,x_star(3,:),'-')
+ylabel('Y [km]')
+subplot(4,1,4); hold on; grid on; grid minor; 
+plot(tvec,x_star(4,:),'-')
+ylabel('Ydot [km/s]')
+
+%full nonlinear measurement data simulation
+figure; hold on; 
+sgtitle('Full Nonlinear Measurement Data Simulation')
+subplot(3,1,1); hold on; grid on; grid minor; ylabel('rho^i [km]')
+subplot(3,1,2); hold on; grid on; grid minor; ylabel('rhodot^i [km/s]')
+subplot(3,1,3); hold on; grid on; grid minor; ylabel('\phi^i [rad]')
+for i=1:12
+    subplot(3,1,1); 
+    plot(tvec,rho(i,:),'x')
+    subplot(3,1,2);
+    plot(tvec,rhoDot(i,:),'o')
+    subplot(3,1,3);
+    plot(tvec,phi(i,:),'o')
+end
+
+%total states vs time for linearized dynamics model
+figure; hold on;
+sgtitle(sprintf('States vs. Time, Linearized Approximate Dynamics Simulation \n dx =[%.4fkm %.4fkm/s %.4fkm %.4fkm/s]',dx0(1),dx0(2),dx0(3),dx0(4)))
 subplot(4,1,1); hold on; grid on; grid minor;
 title('x position [km]')
-plot(tvec,x_sim(:,1),'-')
+plot(tvec,x_sim(1,:),'-')
 subplot(4,1,2); hold on; grid on; grid minor;
 title('x velocity [km/s]')
-plot(tvec,x_sim(:,2),'-')
+plot(tvec,x_sim(2,:),'-')
 subplot(4,1,3); hold on; grid on; grid minor;
 title('y position [km]')
-plot(tvec,x_sim(:,3),'-')
+plot(tvec,x_sim(3,:),'-')
 subplot(4,1,4); hold on; grid on; grid minor;
 title('y velocity [km/s]')
-plot(tvec,x_sim(:,4),'-')
+plot(tvec,x_sim(4,:),'-')
 
+%linearized approximate solution
+figure; hold on; 
+sgtitle('States vs. Time, Linearized Approximate Dynamics Simulation')
+subplot(4,1,1); hold on; grid on; grid minor;
+plot(tvec,dx_lin(1,:),'-')
+ylabel('X [km]')
+subplot(4,1,2); hold on; grid on; grid minor;
+plot(tvec,dx_lin(2,:),'-')
+ylabel('Xdot [km/s]')
+subplot(4,1,3); hold on; grid on; grid minor;
+plot(tvec,dx_lin(3,:),'-')
+ylabel('Y [km]')
+subplot(4,1,4); hold on; grid on; grid minor;
+plot(tvec,dx_lin(4,:),'-')
+ylabel('Ydot [km/s]')
 
-
+%linearized approximate measurement simulation
+figure; hold on;
+sgtitle('Approximate Linearized Measurement Data Simulation')
+subplot(3,1,1); hold on; grid on; grid minor; ylabel('rho^i [km]')
+subplot(3,1,2); hold on; grid on; grid minor; ylabel('rhodot^i [km/s]')
+subplot(3,1,3); hold on; grid on; grid minor; ylabel('\phi^i [rad]')
+for i=1:12
+    subplot(3,1,1); 
+    plot(tvec,y_sim(3*i-2,:),'x')
+    subplot(3,1,2);
+    plot(tvec,y_sim(3*i-1,:),'o')
+    subplot(3,1,3);
+    plot(tvec,y_sim(3*i,:),'o')
+end
 
 %propagation function
 function [ ds ] = orbit_prop_func(t,s)
@@ -187,7 +250,7 @@ end
 
 function [F Omega] = F_Gamma_variant(X,Y)
 mu = 398600;        % km^3/s^2
-r0_nom = 6678;          % km
+r0_nom = sqrt(X^2+Y^2);          % km
 dt = 10;
 
 A = [0, 1, 0, 0;
